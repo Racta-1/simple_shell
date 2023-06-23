2a34485ba7b274148bf437144772b8e8609edeb9
Akinrele Oluwabusayo Taye <36778872+Racta-1@users.noreply.github.com>
2023-06-23 09:27:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - the main function (entry point)
- * @ac: arg count
- * @av: arg vector
+ * @count: arg count
+ * @vec: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+int main(int count, char **vec)
 {
-	info_t info[] = { INFO_INIT };
+	info_t var[] = { INFO_INIT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
@@ -17,29 +17,29 @@ int main(int ac, char **av)
 			: "=r" (fd)
 			: "r" (fd));
 
-	if (ac == 2)
+	if (count == 2)
 	{
-		fd = open(av[1], O_RDONLY);
+		fd = open(vec[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(vec[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(vec[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		var->readfd = fd;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	populate_env_list(var);
+	read_history(var);
+	hsh(var, vec);
 	return (EXIT_SUCCESS);
 }
 
