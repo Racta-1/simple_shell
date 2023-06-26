df25c635fd3904dbf51a030b94c603ed6beae5ee
Akinrele Oluwabusayo Taye <36778872+Racta-1@users.noreply.github.com>
2023-06-26 19:57:39 +0100

    Update interactive_shell.c

--- interactive_shell.c
+++ interactive_shell.c
@@ -1,45 +1,47 @@
 #include "shellheader.h"
+
 /**
- * interactive - returns true if shell is interactive mode
+ * interactive - a function that returns true if shell is interactive mode
  * @var: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
+
 int interactive(info_t *var)
 {
 	return (isatty(STDIN_FILENO) && var->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
+ * is_delim - a function that checks if character is a delimeter
+ * @char: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delim(char char, char *delim)
 {
 	while (*delim)
-		if (*delim++ == c)
+		if (*delim++ == char)
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * _isalpha - a function that checks for alphabetic character
+ * @char: The character to input
+ * Return: 1 if char is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
+int _isalpha(int char)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - a function that converts a string to an integer
  * @str: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
